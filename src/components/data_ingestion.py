8bac27db72e2f06e17847eb88add1a46d11e13a9
Riaz Ahamed <riazlearningaiml@gmail.com>
2026-06-10 22:51:24 +0300

    data transformation processed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,7 +4,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 @dataclass
 class DataIngestionConfig:
     raw_data_path: str = os.path.join('artifacts', 'raw_data.csv')
@@ -40,4 +41,7 @@ class DataIngestion:
         
 if __name__ == '__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
---
+++ src/components/utils.py
@@ -0,0 +1,17 @@
+import os 
+import pandas as pd
+import numpy as np
+import pickle
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        print('Error in saving object')
+        raise e
+    
